589c12adfb519fbfa96989254e3a312d761608df
Tyler Mandry <tmandry@google.com>
2020-01-23 22:08:40 -0800

    Remove RevPath

--- src/hir.rs
+++ src/hir.rs
@@ -11,7 +11,7 @@ use thiserror::Error;
 #[derive(Error, Debug)]
 pub(crate) enum LookupError {
     #[error("couldn't find `{0}`")]
-    NotFound(RevPath),
+    NotFound(Path),
     #[error("{0}")]
     SourceError(clang::SourceError),
 }
@@ -61,50 +61,26 @@ impl From<String> for Path {
     }
 }
 impl Path {
-    fn iter(&self) -> impl Iterator<Item = &Ident> {
+    fn dummy() -> Path {
+        Path { components: vec![] }
+    }
+    pub(crate) fn iter(&self) -> impl Iterator<Item = &Ident> {
         self.components.iter()
     }
 }
 impl Display for Path {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write_path(self.iter(), f)
-    }
-}
-
-// A path stored in reverse; only used in LookupError::NotFound.
-#[derive(Clone, Debug)]
-pub(crate) struct RevPath {
-    rev_components: Vec<Ident>,
-}
-impl RevPath {
-    fn new() -> RevPath {
-        RevPath {
-            rev_components: vec![],
+        let mut iter = self.components.iter();
+        let mut next = iter.next();
+        while let Some(id) = next {
+            write!(f, "{}", id)?;
+            next = iter.next();
+            if next.is_some() {
+                write!(f, "::")?;
+            }
         }
+        Ok(())
     }
-    fn push_front(&mut self, id: Ident) {
-        self.rev_components.push(id);
-    }
-}
-impl Display for RevPath {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write_path(self.rev_components.iter().rev(), f)
-    }
-}
-
-fn write_path<'a>(
-    mut iter: impl Iterator<Item = &'a Ident>,
-    f: &mut fmt::Formatter,
-) -> fmt::Result {
-    let mut next = iter.next();
-    while let Some(id) = next {
-        write!(f, "{}", id)?;
-        next = iter.next();
-        if next.is_some() {
-            write!(f, "::")?;
-        }
-    }
-    Ok(())
 }
 
 pub(crate) struct HirNode<'tu> {
@@ -146,7 +122,12 @@ impl<'tu> Hir<'tu> {
     pub(crate) fn lookup_id(&mut self, path: &Path) -> Result<NodeId> {
         let mut cur = NodeId(0);
         for name in path.iter() {
-            cur = self.child_of(cur, name)?;
+            cur = match self.child_of(cur, name) {
+                Ok(child) => child,
+                // Fill in the path we failed to find.
+                Err(LookupError::NotFound(_)) => return Err(LookupError::NotFound(path.clone())),
+                Err(e) => return Err(e),
+            }
         }
         Ok(cur)
     }
@@ -158,7 +139,7 @@ impl<'tu> Hir<'tu> {
         let children = self.node(node).items.as_ref().unwrap();
         match children.get(child) {
             Some(id) => Ok(*id),
-            None => Err(LookupError::NotFound(RevPath::new())),
+            None => Err(LookupError::NotFound(Path::dummy())),
         }
     }
 

bf80c59a38840b7e64dcc1680f49121fc8ee4853
Tyler Mandry <tmandry@google.com>
2020-02-01 21:07:54 -0800

    Start adding ir

---
+++ src/ir.rs
@@ -0,0 +1,82 @@
+//! Intermediate representation.
+//!
+//! This module contains all the semantics we can represent in our bindings.
+
+#![allow(dead_code)]
+
+use crate::index::{Ident, Path};
+use std::num::NonZeroU16;
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum Ty {
+    Short,
+    UShort,
+    Int,
+    UInt,
+    Long,
+    ULong,
+    LongLong,
+    ULongLong,
+    /// `char` when the default char type is signed.
+    CharS,
+    /// `char` when the default char type is unsigned.
+    CharU,
+    SChar,
+    UChar,
+
+    Size,
+    SSize,
+    PtrDiff,
+
+    Float,
+    Double,
+    LongDouble,
+
+    Bool,
+}
+
+impl Ty {
+    pub fn is_integral(&self) -> bool {
+        use Ty::*;
+        match self {
+            Short | UShort | Int | UInt | Long | ULong | LongLong | ULongLong | CharS | CharU
+            | SChar | UChar | Size | SSize | PtrDiff => true,
+            Float | Double | LongDouble => false,
+            Bool => false,
+        }
+    }
+
+    pub fn is_floating(&self) -> bool {
+        use Ty::*;
+        match self {
+            Float | Double | LongDouble => true,
+            Short | UShort | Int | UInt | Long | ULong | LongLong | ULongLong | CharS | CharU
+            | SChar | UChar | Size | SSize | PtrDiff => false,
+            Bool => false,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Struct {
+    pub name: Path,
+    pub fields: Vec<Field>,
+    pub offsets: Vec<Offset>,
+    pub repr: Repr,
+    pub align: Align,
+}
+
+#[derive(Debug)]
+pub struct Field {
+    pub name: Ident,
+    pub ty: Ty,
+}
+
+pub type Offset = NonZeroU16;
+pub type Align = NonZeroU16;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Repr {
+    C,
+    Opaque,
+}
--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,16 @@ mod test_util;
 mod util;
 
 mod index;
+mod ir;
 
-use crate::index::Path;
+use crate::index::{Ident, Path};
+use crate::ir::*;
 use crate::util::DisplayName;
 use clang::{
-    self, Clang, Entity, EntityKind, Parser, SourceError, TranslationUnit, Type, TypeKind,
+    self, Accessibility, Clang, Entity, EntityKind, Parser, SourceError, TranslationUnit, Type,
+    TypeKind,
 };
+use std::convert::TryInto;
 use std::env;
 
 fn main() -> Result<(), SourceError> {
@@ -64,7 +68,7 @@ impl<'tu> BindGen<'tu> {
 
         for (name, export) in exports {
             match export {
-                Export::Decl(decl_ref) => self.lower_decl(&name, decl_ref),
+                Export::Decl(decl_ref) => self.lower_decl(name, decl_ref),
                 Export::Type(ty) => {
                     println!("{} = {:?}", name, ty);
                     println!(
@@ -122,7 +126,7 @@ impl<'tu> BindGen<'tu> {
         }
     }
 
-    fn lower_decl(&self, name: &Path, decl_ref: Entity<'tu>) {
+    fn lower_decl(&self, name: Path, decl_ref: Entity<'tu>) {
         let overloads = decl_ref.get_overloaded_declarations().unwrap();
         assert_eq!(overloads.len(), 1);
         let ent = overloads[0];
@@ -138,13 +142,42 @@ impl<'tu> BindGen<'tu> {
         }
     }
 
-    fn lower_struct(&self, name: &Path, ent: Entity<'tu>) {
+    fn lower_struct(&self, name: Path, ent: Entity<'tu>) {
         let ty = ent.get_type().unwrap();
         if !ty.is_pod() {
             // TODO: Proper error handling
             eprintln!("{}: Only POD structs are supported", name);
             return;
         }
+
+        let mut fields = vec![];
+        let mut offsets = vec![];
+
+        // TODO get rid of unwrap()
+        for field in ty.get_fields().unwrap() {
+            if let Some(acc) = field.get_accessibility() {
+                if Accessibility::Public != acc {
+                    continue;
+                }
+            }
+            let field_name = field.get_name().unwrap();
+            let field_ty = field.get_type().unwrap();
+            fields.push(Field {
+                name: Ident::from(field_name),
+                ty: field_ty.lower(),
+            });
+            let offset = field.get_offset_of_field().unwrap().try_into().unwrap();
+            offsets.push(ir::Offset::new(offset).unwrap());
+        }
+        let align = ty.get_alignof().unwrap().try_into().unwrap();
+
+        let _lowered = ir::Struct {
+            name: name.clone(),
+            fields,
+            offsets,
+            repr: ir::Repr::C,
+            align: ir::Align::new(align).unwrap(),
+        };
 
         // TODO: Make not silly =)
         println!("#[repr(C)]");
@@ -186,6 +219,29 @@ impl<'tu> BindGen<'tu> {
             println!(",");
         }
         println!("}}");
+    }
+}
+
+trait Lower {
+    type Output;
+    fn lower(&self) -> Self::Output;
+}
+
+impl<'tu> Lower for Type<'tu> {
+    type Output = Ty;
+    fn lower(&self) -> Ty {
+        use TypeKind::*;
+        match self.get_kind() {
+            Int => Ty::Int,
+            UInt => Ty::UInt,
+            CharS => Ty::CharS,
+            SChar => Ty::SChar,
+            CharU => Ty::CharU,
+            UChar => Ty::UChar,
+            Float => Ty::Float,
+            Double => Ty::Double,
+            _ => panic!("unsupported type {:?}", self),
+        }
     }
 }
 

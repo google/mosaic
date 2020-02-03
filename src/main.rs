87e2d9894634e3b58e03d4106b737ca9a8551f29
Tyler Mandry <tmandry@google.com>
2020-02-02 22:13:33 -0800

    Move AstVisitor to util::debug

--- src/main.rs
+++ src/main.rs
@@ -57,7 +57,7 @@ impl<'tu> BindGen<'tu> {
     }
 
     fn gen(&mut self) -> Result<&mut Self, SourceError> {
-        //let mut visitor = Visitor::new(&self);
+        //let mut visitor = AstVisitor::new(&self);
         let mut exports = vec![];
         for ent in self.tu.get_entity().get_children() {
             if let EntityKind::Namespace = ent.get_kind() {
@@ -209,117 +209,5 @@ impl<'tu> Lower for Type<'tu> {
             Double => Ty::Double,
             _ => panic!("unsupported type {:?}", self),
         }
-    }
-}
-
-#[allow(dead_code)]
-struct Visitor<'cpp> {
-    namespace: String,
-    current_ty: Option<Type<'cpp>>,
-    indent: usize,
-}
-
-#[allow(dead_code)]
-impl<'cpp> Visitor<'cpp> {
-    fn new() -> Visitor<'cpp> {
-        Visitor {
-            namespace: String::new(),
-            current_ty: None,
-            indent: 0,
-        }
-    }
-
-    fn visit_children(&mut self, ent: Entity<'cpp>) {
-        let entities = ent.get_children().into_iter();
-        for ent in entities {
-            use EntityKind::*;
-            match ent.get_kind() {
-                Namespace => self.handle_namespace(ent),
-                StructDecl | ClassDecl => self.handle_struct(ent),
-                FieldDecl => self.handle_field(ent),
-                Method => self.handle_method(ent),
-                TypedefDecl => self.handle_typedef(ent),
-                ClassTemplate => self.handle_class_template(ent),
-                _ => (),
-            }
-        }
-    }
-
-    fn handle_namespace(&mut self, ns: Entity<'cpp>) {
-        let full = format!("{}::{}", self.namespace, ns.display_name());
-        let orig = std::mem::replace(&mut self.namespace, full);
-        self.visit_children(ns);
-        self.namespace = orig;
-    }
-
-    fn handle_struct(&mut self, st: Entity<'cpp>) {
-        print_indent!(self, "struct {}::{}", self.namespace, st.display_name());
-        let ty = st.get_type().unwrap();
-        match ty.get_sizeof() {
-            Ok(size) => println!(" ({} bytes)", size),
-            Err(e) => println!(" ({})", e),
-        }
-
-        self.indent += 1;
-        self.visit_with_ty(Some(ty), st);
-        self.indent -= 1;
-    }
-
-    fn handle_field(&mut self, field: Entity<'cpp>) {
-        match field.get_name() {
-            Some(name) => {
-                print_indent!(self, "{}", field.display_name());
-                if let Some(ty) = field.get_type() {
-                    print!(": {}", ty.display_name());
-                }
-                if let Some(struct_ty) = self.current_ty {
-                    match struct_ty.get_offsetof(&name) {
-                        Ok(offset) => print!(" (offset {} bits)", offset),
-                        Err(e) => print!(" ({})", e),
-                    }
-                }
-                println!();
-            }
-            None => {
-                println!("field: {}", field.display_name());
-            }
-        }
-    }
-
-    fn handle_method(&mut self, meth: Entity<'cpp>) {
-        print_indent!(self, "{}", meth.display_name());
-        if let Some(ty) = meth.get_result_type() {
-            print!(" -> {}", ty.display_name());
-        }
-        println!();
-    }
-
-    fn handle_typedef(&mut self, td: Entity<'cpp>) {
-        println_indent!(
-            self,
-            "typedef {} = {}",
-            td.display_name(),
-            td.get_typedef_underlying_type().unwrap().display_name()
-        );
-    }
-
-    fn handle_class_template(&mut self, t: Entity<'cpp>) {
-        println_indent!(self, "template {}::{}", self.namespace, t.display_name());
-        let name = format!(
-            "{}::{}",
-            self.namespace,
-            t.get_name().unwrap_or("?".to_string())
-        );
-        println_indent!(self, "  => {}", name);
-
-        self.indent += 1;
-        self.visit_with_ty(None, t);
-        self.indent -= 1;
-    }
-
-    fn visit_with_ty(&mut self, ty: Option<Type<'cpp>>, ent: Entity<'cpp>) {
-        let orig = std::mem::replace(&mut self.current_ty, ty);
-        self.visit_children(ent);
-        self.current_ty = orig;
     }
 }

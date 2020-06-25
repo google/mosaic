804add82f486a41fd307bc6445d754028bd8e9ab
Tyler Mandry <tmandry@google.com>
2020-06-24 22:27:35 -0700

    Use compiletest tools.mk from run-make tests

--- test/run-make/common.mk
+++ test/run-make/common.mk
@@ -1,24 +1,21 @@
 RUSTC   ?= rustc
-CC      ?= cc
-AR      ?= ar
 TMPDIR  ?= /youneedtosupply/tmpdir
 BINDGEN ?= cargo run --bin=peasy --
-EXTRACXXRSFLAGS := -lstdc++
 
-BARE_RUSTC := $(RUSTC)
-RUSTC      := $(BARE_RUSTC) --out-dir $(TMPDIR) -L $(TMPDIR)
+LD_LIB_PATH_ENVVAR ?= LD_LIBRARY_PATH
+include ../../../third_party/rust/run-make-fulldeps/tools.mk
+
+CXX := $(CXX) -I.
 
 BARE_BINDGEN := $(BINDGEN)
 BINDGEN      := $(BARE_BINDGEN) --out-dir $(TMPDIR)
 
 $(TMPDIR)/lib%.o: %.cc
-	$(CC) -c -o $@ -I. $<
+	$(call COMPILE_OBJ_CXX,$@,$<)
 $(TMPDIR)/lib%.o: $(TMPDIR)/%.cc
-	$(CC) -c -o $@ -I. $<
-%.a: %.o
-	$(AR) crs $@ $<
+	$(call COMPILE_OBJ_CXX,$@,$<)
 
 $(TMPDIR)/%_bind.rs $(TMPDIR)/%_bind.cc: %.h
 	$(BINDGEN) $<
-%_bind.rlib: %_bind.rs lib%_bind.a
-	$(RUSTC) --crate-type=lib -lstatic=$(notdir $*)_bind $(EXTRACXXRSFLAGS) $<
+$(TMPDIR)/%_bind.rlib: $(TMPDIR)/%_bind.rs $(TMPDIR)/lib%_bind.a
+	$(RUSTC) --crate-type=lib -lstatic=$(notdir $*)_bind $(EXTRARSCXXFLAGS) $<
